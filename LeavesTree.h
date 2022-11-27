65d684253247badd2e25427eae531c0abb10a7e6
Enzo Zavorski Delevatti <46092863+Zvorky@users.noreply.github.com>
2022-11-27 01:28:32 -0300

    Structs and search()

--- LeavesTree.h
+++ LeavesTree.h
@@ -17,27 +17,68 @@
 /                           */
 
 /*
-    Non-Nodes Tree. At real
-   it's a list of pairs that
-    acts like a binary tree
+    A Binary Tree...
+
+           ...but:
+      Nodes contains just
+    addresses and optionall
+        secundary data.
+    
+    Main data goes always
+        in the Leaves.
                             */
 
 
-#include <vector>
 
-
-template <typename type>
-struct Leaf{
-    std::vector<bool> key;
-    type data;  
-
-    Leaf(Leaf(std::vector<bool> leaf_key, type leaf_data)){
-        key  = leaf_key;
-        data = leaf_data;
+template <typename key_type>
+struct Key{
+    key_type key;
+    Key(key_type k){
+        key = k;
     }
 };
 
-template <typename type>
-struct Tree{
-    Leaf<type> * leaves = NULL;
+
+template <typename key_type, typename data_type>
+struct Leaf : Key<key_type>{
+    data_type data = 0;
 };
+
+
+template <typename key_type, typename secundary_type = void>
+struct Node : Key<key_type>{
+    Key<key_type> * left = NULL, * right = NULL;
+    secundary_type data;
+};
+
+
+
+template <typename key_type, typename data_type>
+Leaf<key_type, data_type> * search(Node<key_type> * root, key_type key){
+    if(!root)
+        return NULL;
+    
+    Node<key_type> * nav = root;
+    while(nav && key){
+        if(key && 0b1)
+            nav = nav->right;
+        else
+            nav = nav->left;
+        key = key >> 1;
+    }
+    
+    return nav;
+}
+
+
+//  ! incomplete !
+template <typename key_type, typename data_type>
+bool add(Leaf<key_type, data_type> root, data_type data, bool add_at_right = true){
+    if(!root)
+        return false;
+    
+    key_type key = (root.key << 1) + 1 && add_at_right;
+    
+    Leaf<key_type, data_type> * a = root;
+    
+}
