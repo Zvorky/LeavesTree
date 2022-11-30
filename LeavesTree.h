2890b6505b832b0bee1a55200b89df14d59a7351
Enzo Zavorski Delevatti <enzzavorski@edu.univali.br>
2022-11-30 14:23:57 -0300

    Returned to old Node struct only architecture

--- LeavesTree.h
+++ LeavesTree.h
@@ -20,122 +20,126 @@
     A Binary Tree...
 
            ...but:
-      Nodes contains just
-    addresses and optionall
-        secundary data.
-    
-    Main data goes always
+     Main data goes always
         in the Leaves.
+
+        Other Nodes are
+     suposed to have only
+    addresses and optional
+        secundary data.
                             */
 
 
 
-template <typename key_type>
-struct Key{
-    key_type key;
-    Key(key_type k){
-        key = k;
+template <typename key_type, typename data_type, typename secundary_type>
+struct Node{
+    key_type          key;
+    data_type       * data;
+    secundary_type  * secundary_data;
+    Node<key_type, data_type, secundary_type> * left, * right;
+
+    //  Key only
+    Node(key_type key = 0){
+        this->key   = key;
+    }
+
+    //  Path Node
+    Node(key_type key, secundary_type secundary_data){
+        this->left  = NULL;
+        this->right = NULL; 
+        this->key   = key;
+       *this->secundary_data = secundary_data;
+    }
+
+    //  Leaf
+    Node(key_type key, data_type data, secundary_type secundary_data = NULL){
+        this->key   = key;
+        this->data  = new data_type;
+       *this->data  = data;
+        this->secundary_data = new secundary_type;
+       *this->secundary_data = secundary_data;
     }
 };
 
 
-template <typename key_type, typename data_type>
-struct Leaf : Key<key_type>{
-    data_type data = 0;
 
-    Leaf(key_type key) : Key<key_type>(key){}
-};
-
-
-template <typename key_type, typename secundary_type = void>
-struct Node : Key<key_type>{
-    Key<key_type> * left = NULL, * right = NULL;
-    secundary_type data = NULL;
-
-    Node(key_type key){
-        this->key = key;
-    }
-};
-
-
-
-template <typename key_type, typename data_type>
-Leaf<key_type, data_type> * search(Node<key_type> * root, key_type key){
-    if(!root)
-        return NULL;
+// template <typename key_type, typename data_type, typename secundary_type>
+// Node<key_type, data_type, secundary_type> * search(Node<key_type, data_type, secundary_type> * root, key_type key){
+//     if(!root)
+//         return NULL;
     
-    Node<key_type> * nav = root;
-    while(nav && key){
-        if(key && 0b1)
-            nav = nav->right;
-        else
-            nav = nav->left;
-        key = key >> 1;
-    }
+//     Node<key_type, data_type, secundary_type> * nav = root;
+//     while(nav && key){
+//         if(key && 0b1)
+//             nav = nav->right;
+//         else
+//             nav = nav->left;
+//         key = key >> 1;
+//     }
     
-    return nav;
-}
+//     return nav;
+// }
 
 
-//  Create new Leaf beside another
-//  ! incomplete !
-template <typename key_type, typename data_type, typename secundary_type = void>
-bool add(Key<key_type> * node, data_type data, secundary_type node_data = NULL, bool add_at_right = true){
-    if(!node){
-        node = new Leaf<key_type, data_type>(node->key);
-        node.data = data;
-        return true;
-    }
+// //  Create new Leaf beside another
+// //  ! incomplete !
+// template <typename key_type, typename data_type, typename secundary_type>
+// bool add(Node<key_type, data_type, secundary_type> * node, data_type data, secundary_type node_data = NULL, bool add_at_right = true){
+//     if(!node){
+//         node = new Node<key_type, data_type, secundary_type>(0, data);
+//         return true;
+//     }
 
-    key_type key = node->key << 1;
-    Node<key_type> * nav = node;
+//     key_type key = node->key << 1;
+//     Node<key_type, data_type, secundary_type> * nav = node;
 
-    if(add_at_right){
+//     if(add_at_right){
 
-        //  I think it gonna except, a Leaf dont have 'right'
-        //  Make sure its at the
-        while(nav->right){
-            // key = key << 1 + 1;
-            nav = nav->right;
-        }
+//         //  I think it gonna except, a Leaf dont have 'right'
+//         //  Make sure its at the
+//         while(nav->right){
+//             // key = key << 1 + 1;
+//             nav = nav->right;
+//         }
         
-        Leaf<key_type, data_type> * existent = * nav->right;
-        nav = nav->right;
+//         Node<key_type, data_type, secundary_type> * existent;
+//         * existent = * nav->right;
+//         nav = nav->right;
 
-        //  A new Node get the position
-        nav = new Node<key_type, secundary_type>(nav->right->key);
-        nav.data = node_data;
+//         //  A new Node get the position
+//         nav = new Node<key_type, data_type, secundary_type>(nav->right->key, node_data);
 
-        //  Existent Leaf assumes its new position
-        nav->left = existent;
-        existent->key = existent->key << 1;
+//         //  Existent Leaf assumes its new position
+//         nav->left = existent;
+//         existent->key = existent->key << 1;
 
-        //  Finally, the new Leaf
-        nav->right = new Leaf<key_type, data_type>(nav->key << 1 + 1);
-        nav->right->data = data;
+//         //  Finally, the new Leaf
+//         nav->right = new Node<key_type, data_type, secundary_type>(nav->key << 1 + 1, data);
         
-        return true;
-    // // It needs optimization
-    // }else{
-    //     while(nav->left){
-    //         nav = nav->left;
-    //     }
-    }
-}
+//         return true;
+//     // // It needs optimization
+//     // }else{
+//     //     while(nav->left){
+//     //         nav = nav->left;
+//     //     }
+//     }
+
+//     return false;
+// }
 
 
-template <typename key_type, typename data_type>
-bool swap(Leaf<key_type, data_type> * a, Leaf<key_type, data_type> * b){
-    if(!a || !b)
-        return false; // Can't swap with NULL, it would be a move()
+// template <typename key_type, typename data_type, typename secundary_type>
+// bool swap(Node<key_type, data_type, secundary_type> * a, Node<key_type, data_type, secundary_type> * b){
+//     if(!a || !b)
+//         return false; // Can't swap with NULL, it would be a move()
     
-    Leaf<key_type, data_type> * swap = * a;
+//     Node<key_type, data_type, secundary_type> * swap = * a;
     
-    swap->key   = b->key;
-    b->key      = a->key;
+//     swap->key   = b->key;
+//     b->key      = a->key;
 
-    * a = * b;
-    * b = swap;
+//     * a = * b;
+//     * b = swap;
     
-    return true;
-}
+//     return true;
+// }
